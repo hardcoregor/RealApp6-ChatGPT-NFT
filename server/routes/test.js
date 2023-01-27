c175ff58794563621f9c9c152db7070ff425f43f
hardcoregor <spraytown2014@gmail.com>
2023-01-27 05:28:00 +0200

    deploy

---
+++ server/routes/test.js
@@ -0,0 +1,7 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+console.log(process.env.CLOUDINARY_CLOUD_NAME)
+console.log(process.env.CLOUDINARY_API_KEY)
+console.log(process.env.CLOUDINARY_API_SECRET)
+console.log(process.env.OPENAI_API_KEY)
